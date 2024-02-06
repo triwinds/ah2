4bdb43c56fdbc60f995c3fc936dc1216440aaf3c
triwinds <triwinds@users.noreply.github.com>
2024-02-06 20:56:06 +0800

    add drones

--- Arknights/addons/contrib/maa/__init__.py
+++ Arknights/addons/contrib/maa/__init__.py
@@ -82,7 +82,7 @@ def maa_infrast(asst: Asst):
             "Mfg", "Trade", "Control", "Power", "Reception", "Office", "Dorm"
         ],
         # "_NotUse"、"Money"、"SyntheticJade"、"CombatRecord"、"PureGold"、"OriginStone"、"Chip"
-        'drones': "SyntheticJade",
+        'drones': "Money",
         "replenish": True
     })
 
