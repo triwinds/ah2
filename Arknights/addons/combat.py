01ccb1574a77007c1d7e807a66d6c50814f2b4ae
triwinds <triwinds@users.noreply.github.com>
2024-02-16 21:11:29 +0800

    fix count

--- Arknights/addons/combat.py
+++ Arknights/addons/combat.py
@@ -414,7 +414,7 @@ class CombatAddon(AddonBase):
             remain = desired_count - count
             if remain > 1:
                 self.logger.error('已忽略余下的 %d 次战斗', remain - 1)
-        self.stage_count[c_id] = remain
+        self.stage_count[c_id] = count
         return c_id, remain
 
     def can_perform_refill(self):
