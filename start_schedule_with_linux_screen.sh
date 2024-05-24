16d3e9f304ac3b62d55645c7443594cf08d05e5e
triwinds <triwinds@users.noreply.github.com>
2024-05-24 19:52:55 +0800

    kill last process before run script

--- start_schedule_with_linux_screen.sh
+++ start_schedule_with_linux_screen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+kill $(ps aux | grep '[p]ython linux_schedule.py' | awk '{print $2}')
 export HTTP_PROXY=http://127.0.0.1:7890
 export HTTPS_PROXY=http://127.0.0.1:7890
 screen -S ah2 bash -c "cd /root/ah2 && poetry run python linux_schedule.py"
