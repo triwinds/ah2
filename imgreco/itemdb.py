f1a070459b64715e3da70d76526a7bfc70dc173b
triwinds <triwinds@users.noreply.github.com>
2024-11-02 21:17:34 +0800

    call update_extra_items if img  removed

--- imgreco/itemdb.py
+++ imgreco/itemdb.py
@@ -92,8 +92,12 @@ def update_net():
         resp = retry_get('https://gh.cirno.xyz/raw.githubusercontent.com/triwinds/arknights-ml/master/inventory/ark_material.onnx')
         with open(net_file, 'wb') as f:
             f.write(resp.content)
+        flag = False
         for file in app.extra_items_path.glob('*.png'):
             file.unlink()
+            flag = True
+        if flag:
+            update_extra_items()
     else:
         os.utime(material_model_gen_time_file, None)
 
