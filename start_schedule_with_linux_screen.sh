e69ad6633ac371cf1c51e075fbc14be929de2440
triwinds <triwinds@users.noreply.github.com>
2025-02-13 21:08:28 +0800

    refactor: 使用 uv 代替 poetry 运行 linux_schedule.py
    - 将屏幕会话中的命令从 poetry run python linux_schedule.py 更改为 uv run python linux_schedule.py
    - 此更改可能会影响项目的依赖管理和虚拟环境配置

--- start_schedule_with_linux_screen.sh
+++ start_schedule_with_linux_screen.sh
@@ -2,4 +2,4 @@
 kill $(ps aux | grep '[p]ython linux_schedule.py' | awk '{print $2}')
 export HTTP_PROXY=http://127.0.0.1:7890
 export HTTPS_PROXY=http://127.0.0.1:7890
-screen -S ah2 bash -c "cd /root/ah2 && poetry run python linux_schedule.py"
+screen -S ah2 bash -c "cd /root/ah2 && uv run python linux_schedule.py"
