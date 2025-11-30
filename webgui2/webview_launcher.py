75e45d2c9c9474534e5313176c9a9d76671827be
triwinds <triwinds@users.noreply.github.com>
2025-11-30 21:24:53 +0800

    remove webui2

--- webgui2/webview_launcher.py
+++
@@ -1,4 +0,0 @@
-def launch(url, width, height, gui):
-    import webview
-    window = webview.create_window(title="Arknights Auto Helper", url=url, width=width, height=height, text_select=True)
-    webview.start(gui=gui, debug=True)
